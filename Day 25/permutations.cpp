68f8c09475998568426d39ed197acd94ca0b0576
Pritam kumar <92274739+pritamchaudhary@users.noreply.github.com>
2023-06-28 21:40:22 +0530

    Day 25 added

---
+++ Day 25/permutations.cpp
@@ -0,0 +1,33 @@
+// Leetcode 46. Permutations
+// Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+
+private:
+    void solve(vector<int> nums, vector<vector<int>>& ans, int index){
+        //base case
+        if(index >= nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int j=index; j<nums.size(); j++){
+            swap(nums[index], nums[j]);
+            solve(nums, ans, index+1);
+            //backtrack
+            swap(nums[index], nums[j]);
+        }
+    }
+
+
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int index = 0;
+        solve(nums, ans, index);
+        return ans;
+    }
+};

87d24300c9d8c3fc1b58e235b59648bc1c1eca7e
Pritam kumar <92274739+pritamchaudhary@users.noreply.github.com>
2023-07-16 21:31:20 +0530

    Day 43 added

---
+++ Day 43/minimumNoOfDaysToMakeMBouquets.cpp
@@ -0,0 +1,61 @@
+// Leetcode 1482. Minimum Number of Days to Make m Bouquets
+
+// You are given an integer array bloomDay, an integer m and an integer k.
+
+// You want to make m bouquets. To make a bouquet, you need to use k adjacent flowers from the garden.
+
+// The garden consists of n flowers, the ith flower will bloom in the bloomDay[i] and then can be used in exactly one bouquet.
+
+// Return the minimum number of days you need to wait to be able to make m bouquets from the garden. If it is impossible to make m bouquets return -1.
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+private:
+    bool possible(vector<int> &arr, int day, int m, int k) {
+    int n = arr.size(); //size of the array
+    int cnt = 0;
+    int noOfB = 0;
+    // count the number of bouquets:
+    for (int i = 0; i < n; i++) {
+        if (arr[i] <= day) {
+            cnt++;
+        }
+        else {
+            noOfB += (cnt / k);
+            cnt = 0;
+        }
+    }
+    noOfB += (cnt / k);
+    return noOfB >= m;
+}
+
+public:
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        long long val = m * 1ll * k * 1ll;
+        int n = bloomDay.size();
+
+        //impossible case.
+        if (val > n) return -1;
+        
+        //find maximum and minimum:
+        int mini = INT_MAX, maxi = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            mini = min(mini, bloomDay[i]);
+            maxi = max(maxi, bloomDay[i]);
+        }
+
+        //apply binary search:
+        int low = mini, high = maxi;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (possible(bloomDay, mid, m, k)) {
+                high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+        return low;
+    }
+};

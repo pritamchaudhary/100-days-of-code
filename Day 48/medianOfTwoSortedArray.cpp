9360fd7cb188eb58fd316ce1a68de12e31c21c74
Pritam kumar <92274739+pritamchaudhary@users.noreply.github.com>
2023-07-21 19:09:10 +0530

    Day 48 added

---
+++ Day 48/medianOfTwoSortedArray.cpp
@@ -0,0 +1,44 @@
+// Leetcode 4. Median of Two Sorted Arrays
+
+// Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+// The overall run time complexity should be O(log (m+n)).
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+        if(m>n)
+            //ensuring that binary search happens on minimum size array
+            return findMedianSortedArrays(nums2,nums1);
+            
+        int low=0;
+        int high=m;
+        int medianPos=((m+n)+1)/2;
+
+        while(low<=high) {
+            int cut1 = (low+high)>>1;
+            int cut2 = medianPos - cut1;
+            
+            int l1 = (cut1 == 0)? INT_MIN:nums1[cut1-1];
+            int l2 = (cut2 == 0)? INT_MIN:nums2[cut2-1];
+            int r1 = (cut1 == m)? INT_MAX:nums1[cut1];
+            int r2 = (cut2 == n)? INT_MAX:nums2[cut2];
+            
+            if(l1<=r2 && l2<=r1) {
+                if((m+n)%2 != 0)
+                    return max(l1,l2);
+                else 
+                    return (max(l1,l2)+min(r1,r2))/2.0;
+            }
+            else if(l1>r2) high = cut1-1;
+            else low = cut1+1;
+        }
+        return 0.0;
+    }
+};

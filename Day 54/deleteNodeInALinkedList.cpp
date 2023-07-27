0f1a9269ad45a7ef88e362cad752cbcacf2d647c
Pritam kumar <92274739+pritamchaudhary@users.noreply.github.com>
2023-07-27 17:59:44 +0530

    Day 54 added

---
+++ Day 54/deleteNodeInALinkedList.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* curr = node;
+        curr -> val = curr-> next -> val;
+        curr -> next = curr -> next -> next;
+    }
+};

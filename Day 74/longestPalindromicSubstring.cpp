af30e499b8a45f4efb64f08ee28cc5d15a481ad4
Pritam kumar <92274739+pritamchaudhary@users.noreply.github.com>
2023-08-16 21:58:58 +0530

    Day 74 added

---
+++ Day 74/longestPalindromicSubstring.cpp
@@ -0,0 +1,48 @@
+// Leetcode 5. Longest Palindromic Substring
+
+// Given a string s, return the longest palindromic substring in s.
+
+#include<iostream>
+using namespace std;
+
+class Solution {
+private:
+    void chk(int l,int r,string &s,int &max,string &ans){
+        while(l>=0 && r<s.size() && s[l]==s[r]){
+            l--;
+            r++;
+        }
+        l++;
+        r--;
+
+        int a=r-l+1;
+        if(a>max){
+            ans=s.substr(l,a);
+            max=a;
+        }
+    }
+public:
+    string longestPalindrome(string s) {
+        int n=s.size();
+        int max=0;
+        string ans="";
+
+        // if(n==1){
+        //     return s;
+        // }
+
+        for(int i=0;i<n;i++){
+
+            chk(i,i,s,max,ans);
+            if(i==n-1){
+                break;
+            }
+            chk(i,i+1,s,max,ans);
+
+            if(max==n){
+                return s;
+            }
+        }
+        return ans;
+    }
+};

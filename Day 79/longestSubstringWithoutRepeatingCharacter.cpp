8a3747ad85f709c08d6e81b48c23caa840d3082d
Pritam kumar <92274739+pritamchaudhary@users.noreply.github.com>
2023-08-21 19:50:58 +0530

    Day 79 added

---
+++ Day 79/longestSubstringWithoutRepeatingCharacter.cpp
@@ -0,0 +1,22 @@
+// Leetcode 3. Longest Substring Without Repeating Characters
+
+// Given a string s, find the length of the longest substring without repeating characters.
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> dict(256, -1);
+        int maxLen = 0, start = -1;
+        for (int i = 0; i != s.length(); i++) {
+            if (dict[s[i]] > start)
+                start = dict[s[i]];
+            dict[s[i]] = i;
+            maxLen = max(maxLen, i - start);
+        }
+        return maxLen;
+    }
+};

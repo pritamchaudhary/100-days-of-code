6987a357c4d69a5506dee2d630c2f24def32ac00
Pritam kumar <92274739+pritamchaudhary@users.noreply.github.com>
2023-08-28 11:57:52 +0530

    Day 86 added

---
+++ Day 86/lemonadeChange.cpp
@@ -0,0 +1,39 @@
+// Leetcode 860. Lemonade Change
+
+// At a lemonade stand, each lemonade costs $5. Customers are standing in a queue to buy from you and order one at a time (in the order specified by bills). Each customer will only buy one lemonade and pay with either a $5, $10, or $20 bill. You must provide the correct change to each customer so that the net transaction is that the customer pays $5.
+
+// Note that you do not have any change in hand at first.
+
+// Given an integer array bills where bills[i] is the bill the ith customer pays, return true if you can provide every customer with the correct change, or false otherwise.
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int fives = 0, tens = 0;
+        for (int bill : bills) {
+            if (bill == 5) {
+                fives++;
+            } else if (bill == 10) {
+                if (fives == 0) {
+                    return false;
+                }
+                fives--;
+                tens++;
+            } else {
+                if (tens > 0 && fives > 0) {
+                    tens--;
+                    fives--;
+                } else if (fives >= 3) {
+                    fives -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
